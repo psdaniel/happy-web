7c1d6b2ff2beb08de0aea800304e18c28802fd84
Daniel Souza <daniel.n10@hotmail.com>
2020-10-13 16:43:33 -0300

    finalização da primeira etapa

--- src/pages/OrphanageMap.tsx
+++ src/pages/OrphanageMap.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
   return (
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio de Janeiro</strong>
+          <span>Jacarepaguá</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-22.9226073,-42.7564659]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   )
 }
 
